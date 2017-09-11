eae1a539e756c429541aabf1ccae313d192211f3
yanxxit <yanxxit@163.com>
2017-09-11 11:35:15 +0800

    md5
    base64

---
+++ base/base64/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	var nickname = "闫肖肖"
+	//加密
+	nickname = base64.StdEncoding.EncodeToString([]byte(nickname))
+	fmt.Println(nickname)
+	//解密
+	nick_name_baty, err := base64.StdEncoding.DecodeString(nickname)
+	if err == nil {
+		fmt.Println(string(nick_name_baty))
+	}
+}
--- base/md5/main.go
+++ base/md5/main.go
@@ -6,21 +6,25 @@ import (
 	"io"
 )
 
+func MD5Encode(value string) string {
+	data := []byte(value)
+	has := md5.Sum(data)
+	md5str1 := fmt.Sprintf("%x", has) //将[]byte转成16进制
+	return md5str1
+}
+
+func MD5Encode2(value string) string {
+	w := md5.New()
+	io.WriteString(w, value)                 //将str写入到w中
+	md5str1 := fmt.Sprintf("%x", w.Sum(nil)) //w.Sum(nil)将w的hash转成[]byte格式
+	return md5str1
+}
+
 func main() {
 	str := "abc123"
 
-	//方法一
-	data := []byte(str)
-	has := md5.Sum(data)
-	md5str1 := fmt.Sprintf("%x", has) //将[]byte转成16进制
-
-	fmt.Println(md5str1)
+	fmt.Println(MD5Encode(str))
 
 	//方法二
-
-	w := md5.New()
-	io.WriteString(w, str)                   //将str写入到w中
-	md5str2 := fmt.Sprintf("%x", w.Sum(nil)) //w.Sum(nil)将w的hash转成[]byte格式
-
-	fmt.Println(md5str2)
+	fmt.Println(MD5Encode2(str))
 }
