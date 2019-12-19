387e7f4f1a008aa528190f7ddecb256a1b3afad5
yanxxmh <xx.yan@tingo66.com>
2019-12-19 11:03:30 +0800

    mongodb 的增删改查

--- mongo-go-driver/main.go
+++ mongo-go-driver/main.go
@@ -19,16 +19,18 @@ func main() {
 	accuse_log := model.TingoDb.Collection("accuse_log")
 	user := model.TingoDb.Collection("user")
 	ctx := model.Ctx
-	// bson.RawValue{Type: "string",Value:""}
+
 	// 查询一个数据：做一个唯一查询
 	userData := user.FindOne(ctx, bson.M{"usertoken": "349639"})
 	userData1 := user.FindOne(ctx, map[string]string{"usertoken": "349639"})
 	// 将string 转换为ObjectId
 	id, _ := primitive.ObjectIDFromHex("5cfd288afa9aae5de0c25d4b")
+	fmt.Println(id, id.Hex())
+	// 通过ObjectId 获取数据
+	userData2 := accuse_log.FindOne(ctx, bson.M{"_id": id})
 
-	userData2 := user.FindOne(ctx, bson.M{"_id": id})
-
-	fmt.Println(userData.DecodeBytes())
+	// fmt.Println(userData.DecodeBytes())
+	fmt.Println(userData2.DecodeBytes())
 	userMap := bson.M{}
 	userMap1 := bson.M{}
 	userMap2 := bson.M{}
@@ -40,8 +42,10 @@ func main() {
 	fmt.Println("----------", userMap["phonenumber"])         //通过map进行获取字段
 	fmt.Println("----------", userMap["_id"])                 //通过map进行获取字段
 	fmt.Println("----------:userData1", userMap1["nickname"]) //通过map进行获取字段
-	fmt.Println("----------:userData2", userMap2["nickname"]) //通过map进行获取字段
-
+	fmt.Println("----------:userData2", userMap2["content"])  //通过map进行获取字段
+	// ok
+	userData3 := user.FindOne(ctx, bson.M{"_id": userMap["_id"]})
+	fmt.Println(userData3.DecodeBytes())
 	// 1. accuse_log aggregate 聚合查询
 	var pipeline []bson.M
 	pipeline = []bson.M{
@@ -92,12 +96,15 @@ func main() {
 	timestamp := cur.UnixNano() / (1000000 * 1000)
 	fmt.Println(int32(timestamp))
 	// 插入数据
-	//accuse_log.InsertOne(ctx, bson.M{
-	//	"content": "禅师与青年abcd", "type": "golang", "usertoken": "349639",
-	//	"title": "mongo-go-driver", "status": 0, "created": int32(timestamp),
-	//})
+	accuse_log.InsertOne(ctx, bson.M{
+		"content": "即将删除的数据", "type": "golang", "usertoken": "349639",
+		"title": "mongo-go-driver", "status": 0, "created": int32(timestamp),
+	})
+
+	// 删除数据
+	accuse_log.DeleteOne(ctx, bson.M{"content": "即将删除的数据"})
 
 	// 更新数据
-	// accuse_log.FindOneAndUpdate(ctx, json)
-
+	dd := accuse_log.FindOneAndUpdate(ctx, bson.M{"_id": id}, bson.M{"$set": bson.M{"title": "mongo-update-修改内容"}})
+	fmt.Println(dd.DecodeBytes())
 }
